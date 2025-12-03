aa0931edec6c044f6881c5e19e5617ca567f9dc6
ExumaLtd <jonathon@exuma.co.uk>
2025-12-03 12:55:38 +0000

    Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,16 @@
-// pages/_app.js
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Load Montserrat from Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <title>Zelim SWIFT Maintenance Portal</title>
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
-
-export default MyApp;
