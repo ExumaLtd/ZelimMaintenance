6e4d0b421fa14695675ad07a0497f36383803fa7
ExumaLtd <jonathon@exuma.co.uk>
2025-12-11 15:19:09 +0000

    Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 // pages/_app.js
 
-import "../styles/globals.css";
-import "../styles/landing.css";
-import "../styles/maintenance.css";
-import "../styles/checklist.css";
+import "/styles/globals.css";
+import "/styles/landing.css";
+import "/styles/maintenance.css";
+import "/styles/checklist.css";
 
 import Head from "next/head";
 
@@ -11,13 +11,10 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Google Font — VALIDATED */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-
-        {/* Favicon — ensure the file exists */}
         <link rel="icon" href="/favicon/ZelimFavicon_192x192.png" />
       </Head>
 
