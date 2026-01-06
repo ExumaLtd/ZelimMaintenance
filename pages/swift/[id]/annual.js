49070e846858cb2423aa00e5247ae72ecde97569
ExumaLtd <jonathon@exuma.co.uk>
2026-01-06 13:50:38 +0000

    Changes

--- pages/swift/[id]/annual.js
+++ pages/swift/[id]/annual.js
@@ -85,6 +85,7 @@ export default function Annual({ unit, template, allCompanies = [] }) {
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
         />
+        <title>{unit.serial_number} | Annual Maintenance</title>
       </Head>
       <div className="page-wrapper">
         <div className="swift-checklist-container">
