692736193f4c99a5154c5528220bc004f645e6ec
ExumaLtd <jonathon@exuma.co.uk>
2026-01-21 15:37:25 +0000

    Changes

--- pages/swift/[id]/depth.js
+++ pages/swift/[id]/depth.js
@@ -975,7 +975,7 @@ export async function getServerSideProps({ params }) {
 
     const headers = { Authorization: `Bearer ${apiKey}` };
     const unitFormula = encodeURIComponent(`{public_token}='${token}'`);
-    const templateFormula = encodeURIComponent(`{type}='30-month depth'`);
+    const templateFormula = encodeURIComponent(`FIND('depth', LOWER({type}))`);
 
     const urls = [
       `https://api.airtable.com/v0/${baseId}/${tableName}?filterByFormula=${unitFormula}`,
