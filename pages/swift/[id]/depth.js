e779b44ef62451333fbd716a221597d50ca8acf9
ExumaLtd <jonathon@exuma.co.uk>
2026-01-24 10:50:18 +0000

    Changes

--- pages/swift/[id]/depth.js
+++ pages/swift/[id]/depth.js
@@ -6,6 +6,7 @@ import { getCompanyLogoUrl } from '../../../utils/get-company-logo';
 import ImageUploader from '../../../components/image-uploader';
 import { ChevronDown, ChevronUp, Calendar } from "lucide-react";
 import { useAutoSave } from '../../../hooks/use-auto-save';
+import '../../../styles/voice-input.css';
 
 const autoGrow = (e) => {
   const el = e.target || e;
