e63fd00a69ebfd14ed3ed8d015ccbb37b0993e4d
ExumaLtd <jonathon@exuma.co.uk>
2026-02-17 15:09:51 +0000

    Changes

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,10 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 0.5,
+  dsn: "https://f624d351849b22d4fea6e608024b25b2@o4510901536620544.ingest.de.sentry.io/4510901543764048",
+  tracesSampleRate: 1,
   replaysOnErrorSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
+  enableLogs: true,
   integrations: [
     Sentry.replayIntegration(),
   ],
