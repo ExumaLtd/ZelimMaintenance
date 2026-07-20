d8890554399edbba87f46f5676781befc038f2e6
ExumaLtd <jonathon@exuma.co.uk>
2026-07-20 14:59:00 +0100

    style: sentence case 404 copy

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,11 +12,11 @@ export default function NotFound() {
       <div className="swift-main-layout-wrapper">
         <div className="complete-page-wrapper">
           <div className="complete-card">
-            <h1 className="complete-title">404 – Page Not Found</h1>
+            <h1 className="complete-title">404 – Page not found</h1>
             <p className="complete-text">The page you&apos;re looking for doesn&apos;t exist.</p>
 
             <Link href="/" className="return-dashboard-btn">
-              <span className="left">Return to the home page</span>
+              <span className="left">Return to the homepage</span>
               <span className="right">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
                   <path d="M10.1458 7.5L0 7.5L0 5.83333L10.1458 5.83333L5.47917 1.16667L6.66667 0L13.3333 6.66667L6.66667 13.3333L5.47917 12.1667L10.1458 7.5Z" fill="#172F36"/>
